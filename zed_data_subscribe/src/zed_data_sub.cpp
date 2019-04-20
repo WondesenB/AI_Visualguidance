70a055c116c3ee4a322f2e826b63adb83e6c952c
Wondosen <wondebly@gmail.com>
2019-04-20 15:37:14 +0900

    additional zed camera data subscribtion

--- zed_data_subscribe/src/zed_data_sub.cpp
+++ zed_data_subscribe/src/zed_data_sub.cpp
@@ -1,5 +1,6 @@
 // zed stereo camera subscription code here
 
+
 // include
 #include "ros/ros.h"
 #include <sensor_msgs/Image.h>
@@ -12,6 +13,7 @@
 
 #include <string>
 
+#define RAD2DEG 57.295779513
 
 /**
  * Subscriber callbacks
@@ -33,6 +35,69 @@ void camera_depth_callback(const sensor_msgs::Image::ConstPtr& msg)
     ROS_INFO("Center distance : %g m", depths[centerIdx]);
 }
 
+void odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
+
+    // Camera position in map frame
+    double tx = msg->pose.pose.position.x;
+    double ty = msg->pose.pose.position.y;
+    double tz = msg->pose.pose.position.z;
+
+    // Orientation quaternion
+    tf2::Quaternion q(
+        msg->pose.pose.orientation.x,
+        msg->pose.pose.orientation.y,
+        msg->pose.pose.orientation.z,
+        msg->pose.pose.orientation.w);
+
+    // 3x3 Rotation matrix from quaternion
+    tf2::Matrix3x3 m(q);
+
+    // Roll Pitch and Yaw from rotation matrix
+    double roll, pitch, yaw;
+    m.getRPY(roll, pitch, yaw);
+
+    // Output the measure
+    ROS_INFO("Received odom in '%s' frame : X: %.2f Y: %.2f Z: %.2f - R: %.2f P: %.2f Y: %.2f",
+             msg->header.frame_id.c_str(),
+             tx, ty, tz,
+             roll * RAD2DEG, pitch * RAD2DEG, yaw * RAD2DEG);
+}
+
+void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+
+    // Camera position in map frame
+    double tx = msg->pose.position.x;
+    double ty = msg->pose.position.y;
+    double tz = msg->pose.position.z;
+
+    // Orientation quaternion
+    tf2::Quaternion q(
+        msg->pose.orientation.x,
+        msg->pose.orientation.y,
+        msg->pose.orientation.z,
+        msg->pose.orientation.w);
+
+    // 3x3 Rotation matrix from quaternion
+    tf2::Matrix3x3 m(q);
+
+    // Roll Pitch and Yaw from rotation matrix
+    double roll, pitch, yaw;
+    m.getRPY(roll, pitch, yaw);
+
+    // Output the measure
+    ROS_INFO("Received pose in '%s' frame : X: %.2f Y: %.2f Z: %.2f - R: %.2f P: %.2f Y: %.2f",
+             msg->header.frame_id.c_str(),
+             tx, ty, tz,
+             roll * RAD2DEG, pitch * RAD2DEG, yaw * RAD2DEG);
+}
+
+void imageRightRectifiedCallback(const sensor_msgs::Image::ConstPtr& msg) {
+    ROS_INFO("Right Rectified image received from ZED - Size: %dx%d", msg->width, msg->height);
+}
+
+void imageLeftRectifiedCallback(const sensor_msgs::Image::ConstPtr& msg) {
+    ROS_INFO("Left Rectified image received from ZED - Size: %dx%d", msg->width, msg->height);
+}
 
 int main(int argc, char **argv)
 {
@@ -45,7 +110,12 @@ int main(int argc, char **argv)
 
 
   ros::Subscriber sub        = n.subscribe("/zed/depth/depth_registered", 10, camera_depth_callback);
-
+  ros::Subscriber subOdom    = n.subscribe("/zed/odom", 10, odomCallback);
+  ros::Subscriber subPose    = n.subscribe("/zed/pose", 10, poseCallback);
+  ros::Subscriber subRightRectified = n.subscribe("/zed/right/image_rect_color", 10,
+                                      imageRightRectifiedCallback);
+  ros::Subscriber subLeftRectified  = n.subscribe("/zed/left/image_rect_color", 10,
+                                      imageLeftRectifiedCallback);
   ros::spin();
 
 
